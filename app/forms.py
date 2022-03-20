ef3c2f5d2ce768c324311564d7c8dbddc1d43c65
Lejandru Richards <Lejandru13@gmail.com>
2022-03-19 20:47:15 -0500

    Update

--- app/forms.py
+++ app/forms.py
@@ -6,11 +6,11 @@ from wtforms import StringField, SelectField, validators
 from wtforms.validators import DataRequired
 
 class PropertyForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    bedrooms = StringField('Bedrooms', validators=[DataRequired()])
-    bathrooms = StringField('Bathrooms', validators=[DataRequired()])
-    location = StringField('Location', validators=[DataRequired()])
-    price = StringField('Price', validators=[DataRequired()])
-    prop_type = SelectField('Type', choices=[('House'), ('Apartment')])
+    title = StringField('Property Title', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired()])
+    bedrooms = StringField('No. of Rooms', validators=[DataRequired()])
+    bathrooms = StringField('No. of Bathrooms', validators=[DataRequired()])
+    price = StringField('Price', validators=[DataRequired()])
+    prop_type = SelectField('Property Type', choices=[('House'), ('Apartment')])
+    location = StringField('Location', validators=[DataRequired()])
     imageFile = FileField('Photo', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'], 'Appropriate Images Only')])
